59c2d5de9356efff5cda69583ab27854a5f0d1f0
cormmo <sebastian.steiner00@gmail.com>
2026-08-24 14:43:54 +0200

    Initial commit

---
+++ src/main/java/com/bbrz/sebastian/JobSwiperFrontend/JobSwiperFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bbrz.sebastian.JobSwiperFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSwiperFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSwiperFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbrz/sebastian/JobSwiperFrontend/JobSwiperFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbrz.sebastian.JobSwiperFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSwiperFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
